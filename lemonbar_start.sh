40c202189884d5cb814ceb86e2cd01f17fd012e6
jaloaguero <jaloaguero@gmail.com>
2023-05-31 22:10:30 -0500

    source bug fix

--- lemonbar_start.sh
+++ lemonbar_start.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-source ./lemonbar_config.sh
+source lemonbar_config.sh
 #This script killas all instances of lemonbar and its own script itself and runs lemonbar
 
 killall -q lemonbar
 killall -q bar_feeder
 
-bash $lemonbar_path | lemonbar -g $lemonbar_path -p -F $foreground_color -B $background_color -f Terminus-10 -f FontAwesome-10 | $SHELL
+bash $lemonbar_path | lemonbar -g $bar_dimensions -p -F $foreground_color -B $background_color -f Terminus-10 -f FontAwesome-10 | $SHELL
